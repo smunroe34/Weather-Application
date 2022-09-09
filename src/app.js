70d6ba593c7cf7126a32ed459bc4249e519107c8
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-09-08 20:33:47 -0400

    Added function for temp and location

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function displayTemperature (response) {
+  let temperatureElement = document.querySelector ("#today-temp");
+  let cityElement = document.querySelector ("#city");
+  temperatureElement.innerHTML = Math.round (response.data.main.temp);
+  cityElement.innerHTML= response.data.name;
+}
+
+let apiKey = "dc55516602874d9b155a4733747850c9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+
+axios.get (apiUrl).then (displayTemperature);
