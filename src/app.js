9da85191fd701f52526c6d6dd6cd6ef5ed84d523
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-09-09 13:17:04 -0400

    API call for weather description

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 function displayTemperature (response) {
   let temperatureElement = document.querySelector ("#today-temp");
   let cityElement = document.querySelector ("#city");
+  let descriptionElement = document.querySelector ("#description");
   temperatureElement.innerHTML = Math.round (response.data.main.temp);
   cityElement.innerHTML= response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "dc55516602874d9b155a4733747850c9";
