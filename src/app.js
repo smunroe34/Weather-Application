844778218370e1fad7570383cebc415085d0066a
Sasha Munroe <sashalillianmunroe@gmail.com>
2022-09-10 12:26:41 -0400

    Set weather icon for city

--- src/app.js
+++ src/app.js
@@ -15,21 +15,26 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature (response) {
+  console.log  (response.data)
   let temperatureElement = document.querySelector ("#today-temp");
   let cityElement = document.querySelector ("#city");
   let descriptionElement = document.querySelector ("#description");
   let humidityElement = document.querySelector ("#humidity");
   let windElement = document.querySelector ("#wind");
   let dateElement = document.querySelector ("#date");
+  let iconElement = document.querySelector ("#icon");
+  
   temperatureElement.innerHTML = Math.round (response.data.main.temp);
   cityElement.innerHTML= response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round (response.data.wind.speed);
   dateElement.innerHTML = formatDate (response.data.dt * 1000);
+  iconElement.setAttribute ("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 let apiKey = "dc55516602874d9b155a4733747850c9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
+let city = "Paris"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get (apiUrl).then (displayTemperature);
